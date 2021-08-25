a10f9bead518d97d5adce82d278b6eae198130cb
Jahnavi Tadikonda <jahnavi.tadikonda@audreeinfotech.com>
2021-08-25 12:45:52 +0530

    i created new branch

--- WebApplication2Git/Program.cs
+++ WebApplication2Git/Program.cs
@@ -1,16 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication2Git
 {
     public class Program
-    {
+    {/// <summary>
+    /// from new branch1
+    /// </summary>
+    /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
